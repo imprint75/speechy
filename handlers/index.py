365252f6fc14575604e4a64e65dc93ed082fd913
jaychoo <jaychoo@gmail.com>
2016-03-07 23:04:34 -0500

    init files for tornado app

---
+++ handlers/index.py
@@ -0,0 +1,6 @@
+from handlers.base import BaseHandler
+
+
+class IndexHandler(BaseHandler):
+    def get(self):
+        self.render("index.html")
---
+++ settings.py
@@ -0,0 +1,23 @@
+import tornado
+import tornado.template
+import os
+from tornado.options import define, options
+
+# Make filepaths relative to settings.
+path = lambda root, *a: os.path.join(root, *a)
+ROOT = os.path.dirname(os.path.abspath(__file__))
+
+define("port", default=8888, help="run on the given port", type=int)
+define("debug", default=True, help="debug mode")
+
+tornado.options.parse_command_line()
+
+TEMPLATE_ROOT = path(ROOT, 'templates')
+
+
+settings = dict()
+settings['debug'] = options.debug
+settings['port'] = options.port
+settings['cookie_secret'] = "as890fasd90f8asdf9a0-s982rhh"
+settings['xsrf_cookies'] = True
+settings['template_loader'] = tornado.template.Loader(TEMPLATE_ROOT)
