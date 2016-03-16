a8f5d51b784c8280b56418e2521cde758ca58795
sean <imprint75@gmail.com>
2016-03-16 23:10:23 +0200

    bootstrap, progress bars, etc

--- static/audio.js
+++ static/audio.js
@@ -2,36 +2,62 @@ var record = document.getElementById('record');
 var stop = document.getElementById('stop');
 var audio = document.querySelector('audio');
 var container = document.getElementById('container');
+var progress = document.getElementById('progress');
 var isFirefox = !!navigator.mozGetUserMedia;
 var recordAudio, fileName;
 
-function PostBlob(audioBlob, fileName) {
-  var formElement = $("#xsrf").serializeArray();
+function sendFile(formData) {
+  var xhr = new XMLHttpRequest();
+  xhr.addEventListener("load", transferComplete);
+  xhr.upload.addEventListener("progress", updateProgress);
+  xhr.upload.addEventListener("error", transferFailed);
+  xhr.open('POST', '/upload');
+  xhr.send(formData);
+}
+
+function transferComplete() {
+  setTimeout(function(){
+    progress.children[1].style.width = '50%';
+  }, 1000);
+  document.querySelector('h1').innerHTML = 'RecordRTC';
+  var p = document.createElement("P");
+  p.appendChild(document.createTextNode(this.responseText));
+  document.querySelector('#recordedText').appendChild(p);
+  progress.classList.toggle('hidden');
+  progress.children[0].style.width = '0%';
+  progress.children[1].style.width = '0%';
+}
+
+function updateProgress(oEvent) {
+  if (oEvent.lengthComputable) {
+    var percentComplete = ((oEvent.loaded / oEvent.total) * 100) / 2;
+    progress.children[0].style.width = Math.ceil(percentComplete) + '%';
+    if (percentComplete == 50) {
+      setTimeout(function(){
+        progress.children[1].style.width = '10%';
+      }, 1000);
+    }
+  } else {
+    // Unable to compute progress information since the total size is unknown
+    console.log( "updateProgress:: error..." );
+  }
+}
+
+function transferFailed() {
+  console.log( "transferFailed:: error" );
+}
+
+function postBlob(audioBlob, fileName) {
   var formData = new FormData();
   formData.append('filename', fileName);
-  formData.append(formElement[0]['name'], formElement[0]['value']);
+  var xsrfForm = document.getElementById("xsrf").elements;
+  formData.append(xsrfForm['_xsrf'].name, xsrfForm['_xsrf'].value);
   formData.append('file1', audioBlob);
-  $.ajax({
-    url: '/upload',
-    cache: false,
-    contentType: false,
-    data: formData,
-    processData: false,
-    type: 'POST'
-  }).done(function(jqXHR, textStatus, errorThrown) {
-    document.querySelector('h1').innerHTML = 'RecordRTC';
-    console.log('Success! I think you said: ' + jqXHR);
-    var para = document.createElement("P");
-    para.appendChild(document.createTextNode(jqXHR));
-    document.querySelector('#recordedText').appendChild(para);
-  }).fail(function(jqXHR, textStatus, errorThrown) {
-    console.log( "error" );
-  }).always(function(jqXHR, textStatus, errorThrown) {
-    console.log( "complete" );
-  });
+  sendFile(formData);
 }
 
 record.onclick = function() {
+  progress.classList.toggle('hidden');
   record.disabled = true;
   if(window.stream){
     delete window.stream;
@@ -43,7 +69,7 @@ record.onclick = function() {
     window.stream = stream;
     onstream();
   }, function(error) {
-    alert(JSON.stringify(error, null, '\t'));
+    console.log(JSON.stringify(error, null, '\t'));
   });
   window.stream && onstream();
   function onstream() {
@@ -68,7 +94,7 @@ stop.onclick = function() {
     recordAudio.stopRecording(function() {
       window.stream.getAudioTracks().forEach(function(track){track.stop();});
       document.querySelector('h1').innerHTML = 'Got audio...';
-      PostBlob(recordAudio.getBlob(), fileName);
+      postBlob(recordAudio.getBlob(), fileName);
     });
   }
 };
